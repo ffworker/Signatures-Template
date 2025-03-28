647e4b856bf8563650c12163baaba581b9b5921f
ffworker <162129412+ffworker@users.noreply.github.com>
2025-03-28 16:39:23 +0100

    Update example-config.ps1

--- example-config.ps1
+++ example-config.ps1
@@ -1,16 +1,18 @@
-$Server = "your.ad.server.local"
-$DomainSelection = "YourDomain"
-$UserSelection = "ALL"
-$UseOutlookFolder = $false
+$Server = "your.ad.server.local" # Needed to connect to your DC to get the $UserData(sn,givenname,department,description,telephonenumer,mail)
+$DomainSelection = "YourDomain" # Specific the templates to load (if multiple are needed, add multiple)
+$UserSelection = "ALL" # user1, user2
+$UseOutlookFolder = $false # $true = "<C:>\Users\<username>\Appdata\Microsoft\Signatures
 
+#Define your TemplateFolder 
 $TemplateFiles = @{
     "YourDomain" = ".\Templatefiles\YourDomain_template.html"
 }
-
+#Define your Resourcefolder (Images etc.)
 $ResourceFolders = @{
     "YourDomain" = ".\assets\YourDomain"
 }
 
+#Test-Data
 $UserDataOverride = @{
     givenName       = "Max"
     sn              = "Mustermann"
